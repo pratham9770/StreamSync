1e5c7d88cb279717084d10f6ff2208f19ffe8ba4
pratham9770 <118453138+pratham9770@users.noreply.github.com>
2025-02-15 20:44:50 +0530

    Update route.tsx

--- app/app/api/streams/my/route.tsx
+++ app/app/api/streams/my/route.tsx
@@ -1,6 +1,6 @@
 import { prismaClient } from "@/app/lib/db";
 import { getServerSession } from "next-auth";
-import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 
 export async function GET() {
     
