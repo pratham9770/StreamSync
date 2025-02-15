6f0381f35e8d56aef64f52fef2b7bfc2487badb8
pratham9770 <118453138+pratham9770@users.noreply.github.com>
2025-02-15 20:46:44 +0530

    Update route.ts

--- app/app/api/user/route.ts
+++ app/app/api/user/route.ts
@@ -1,6 +1,6 @@
 import { prismaClient } from "@/app/lib/db";
 import { getServerSession } from "next-auth";
-import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 
 export const GET = async () => {
   const session = await getServerSession();
